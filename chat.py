d363beaa19547aa2ada1d7cd58c97f51621f92d0
noldualima <102939348+noldualima@users.noreply.github.com>
2023-07-04 23:27:59 +0700

    Add files via upload

---
+++ chat.py
@@ -0,0 +1,356 @@
+import sys
+import os
+import json
+import uuid
+import logging
+from queue import Queue
+import threading
+import socket
+import base64
+import traceback
+
+class ServerToServerThread(threading.Thread):
+    def __init__(self, chat, target_server, target_port):
+        self.chat = chat
+        self.target_server_address = target_server
+        self.target_server_port = target_port
+        self.queue = Queue()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.sock.connect((self.target_server_address, self.target_server_port))
+        while True:
+            while not self.queue.empty():
+                msg = self.queue.get()
+                self.sock.sendall(msg.encode())
+
+    def put(self, msg):
+        self.queue.put(msg)
+
+class Chat:
+    def __init__(self):
+        self.sessions = {}
+        self.users = {'messi': {'nama': 'Lionel Messi', 'negara': 'Argentina', 'password': 'surabaya', 'incoming': {}, 'outgoing': {}},
+                      'henderson': {'nama': 'Jordan Henderson', 'negara': 'Inggris', 'password': 'surabaya', 'incoming': {}, 'outgoing': {}},
+                      'lineker': {'nama': 'Gary Lineker', 'negara': 'Inggris', 'password': 'surabaya', 'incoming': {}, 'outgoing': {}},
+                      'ronaldo': {'nama': 'Christiano Ronaldo', 'negara': 'Portugal', 'password': 'surabaya', 'incoming': {}, 'outgoing': {}},
+                      'maguire': {'nama': 'Harry Maguire', 'negara': 'Inggris', 'password': 'surabaya', 'incoming': {}, 'outgoing': {}}}
+        self.servers = {'A': ServerToServerThread(self, '127.0.0.1', 8889),
+                        'B': ServerToServerThread(self, '127.0.0.1', 9000),
+                        'C': ServerToServerThread(self, '127.0.0.1', 9001)}
+        self.running_servers = []
+        self.groups = {'groupA': ['messi@A', 'lineker@A', 'maguire@A'],
+                       'groupB': ['messi@A', 'lineker@B', 'maguire@B'],
+                       'groupC': ['messi@A', 'ronaldo@A', 'henderson@B']}
+
+    def proses(self, data):
+        j = data.split(" ")
+        try:
+            command = j[0].strip()
+            if command == 'auth':
+                username = j[1].strip()
+                password = j[2].strip()
+                logging.warning("AUTH: auth {} {}".format(username, password))
+                return self.autentikasi_user(username, password)
+            elif command == 'register':
+                username = j[1].strip()
+                password = j[2].strip()
+                return self.register_user(username, password)
+            elif command == 'logout':
+                tokenid = j[1].strip()
+                return self.logout(tokenid)
+            elif command == 'send':
+                sessionid = j[1].strip()
+                usernameto = j[2].strip()
+                message = ""
+                for w in j[3:]:
+                    message = "{} {}".format(message, w)
+                usernamefrom = self.sessions[sessionid]['username']
+                logging.warning(
+                    "SEND: session {} send message from {} to {}".format(sessionid, usernamefrom, usernameto))
+                return self.send_message(sessionid, usernamefrom, usernameto, message)
+            elif command == 'inbox':
+                sessionid = j[1].strip()
+                username = self.sessions[sessionid]['username']
+                logging.warning("INBOX: {}".format(sessionid))
+                return self.get_inbox(username)
+            elif command == 'sendgroup':
+                sessionid = j[1].strip()
+                group_id = j[2].strip()
+                server_from = j[3].strip()
+                message = ""
+                for w in j[4:]:
+                    message = "{} {}".format(message, w)
+                usernamefrom = self.sessions[sessionid]['username']
+                logging.warning("SEND: session {} sent message from {} to {}".format(sessionid, usernamefrom, group_id))
+                return self.send_group_message(sessionid, usernamefrom, group_id, server_from, message)
+            elif command == 'connect':
+                server_id = j[1].strip()
+                return self.connect(server_id)
+            elif command == 'addserver':
+                server_id = j[1].strip()
+                server_ip = j[2].strip()
+                server_port = int(j[3].strip())
+                return self.add_server(server_id, server_ip, server_port)
+            elif command == 'sendserver':
+                sessionid = j[1].strip()
+                server_id = j[2].strip()
+                usernameto = j[3].strip()
+                message = ""
+                for w in j[4:]:
+                    message = "{} {}".format(message, w)
+                logging.warning("SendServer: session {} sent message from {} to {}@{}".format(sessionid, server_id))
+                return self.send_to_other_server(sessionid, server_id, usernameto, message)
+            elif command == 'server_inbox':
+                username_from = j[1].strip()
+                username_to = j[2].strip()
+                message = ""
+                for w in j[3:]:
+                    message = "{} {}".format(message, w)
+                return self.server_inbox(username_from, username_to, message)
+            elif command == 'sendfile':
+                sessionid = j[1].strip()
+                address = j[2].strip()
+                filename = j[3].strip()
+                address_split = address.split('@')
+                username_to = address_split[0].strip()
+                server_id = address_split[1].strip()
+                return self.send_file(sessionid, username_to, server_id, filename)
+            elif command == 'sendfilegroup':
+                sessionid = j[1].strip()
+                groupid = j[2].strip()
+                filename = j[3].strip()
+                return self.send_file_group(sessionid, groupid, filename)
+            elif command == 'creategroup':
+                group_id = j[1].strip()
+                members = j[2].strip()
+                return self.create_group(group_id, members)
+            else:
+                return {'status': 'ERROR', 'message': '**Protocol Tidak Benar'}
+        except KeyError:
+            return {'status': 'ERROR', 'message': 'Informasi tidak ditemukan'}
+        except IndexError:
+            return {'status': 'ERROR', 'message': '--Protocol Tidak Benar'}
+
+    def autentikasi_user(self, username, password):
+        if username not in self.users:
+            return {'status': 'ERROR', 'message': 'User Tidak Ada'}
+        if self.users[username]['password'] != password:
+            return {'status': 'ERROR', 'message': 'Password Salah'}
+        tokenid = str(uuid.uuid4())
+        self.sessions[tokenid] = {'username': username, 'userdetail': self.users[username]}
+        return {'status': 'OK', 'tokenid': tokenid}
+
+    def register_user(self, username, password):
+        if username in self.users:
+            return {'status': 'ERROR', 'message': 'User Sudah Ada'}
+        self.users[username] = {'nama': username, 'negara': 'Inggris', 'password': password, 'incoming': {},
+                                'outgoing': {}}
+        os.chdir('files/')
+        os.mkdir(username)
+        os.chdir('../')
+        return {'status': 'OK', 'message': 'user registered'}
+
+    def logout(self, tokenid):
+        if tokenid in self.sessions:
+            del self.sessions[tokenid]
+            return {'status': 'OK', 'message': "User logged out"}
+        else:
+            return {'status': 'ERROR', 'message': "Session not found"}
+
+    def get_user(self, username):
+        if username not in self.users:
+            return False
+        return self.users[username]
+
+    def connect(self, server_id):
+        if server_id in self.running_servers:
+            return {'status': 'ERROR', 'message': 'Server {} is already connected'.format(server_id)}
+        else:
+            try:
+                self.servers[server_id].start()
+                self.running_servers.append(server_id)
+                return {'status': 'OK'}
+            except:
+                trace = traceback.format_exc()
+                return {'status': 'ERROR', 'message': trace}
+
+    def add_server(self, server_id, server_ip, server_port):
+        if server_id in self.servers:
+            return {'status': 'ERROR', 'message': 'Server {} is already exist'.format(server_id)}
+
+        self.servers[server_id] = ServerToServerThread(self, server_ip, server_port)
+        return {'status': 'OK'}
+
+    def send_message(self, sessionid, username_from, username_dest, message):
+        if sessionid not in self.sessions:
+            return {'status': 'ERROR', 'message': 'Session Tidak Ditemukan'}
+        s_fr = self.get_user(username_from)
+        s_to = self.get_user(username_dest)
+
+        if s_fr is False or s_to is False:
+            return {'status': 'ERROR', 'message': 'User Tidak Ditemukan'}
+
+        message = {'msg_from': s_fr['nama'], 'msg_to': s_to['nama'], 'msg': message}
+        outqueue_sender = s_fr['outgoing']
+        inqueue_receiver = s_to['incoming']
+        try:
+            outqueue_sender[username_from].put(message)
+        except KeyError:
+            outqueue_sender[username_from] = Queue()
+            outqueue_sender[username_from].put(message)
+        try:
+            inqueue_receiver[username_from].put(message)
+        except KeyError:
+            inqueue_receiver[username_from] = Queue()
+            inqueue_receiver[username_from].put(message)
+        return {'status': 'OK', 'message': 'Message Sent'}
+
+    def send_group_message(self, sessionid, username_from, group_id, server_from, message):
+        if sessionid not in self.sessions:
+            return {'status': 'ERROR', 'message': 'Session Tidak Ditemukan'}
+        if group_id not in self.groups:
+            return {'status': 'ERROR', 'message': 'Group Tidak Ditemukan'}
+        s_fr = self.get_user(username_from)
+        if s_fr is False:
+            return {'status': 'ERROR', 'message': 'User Tidak Ditemukan'}
+        if username_from + '@' + server_from not in self.groups[group_id]:
+            return {'status': 'ERROR', 'message': 'User Tidak Ada dalam {}'.format(group_id)}
+        sent_id = []
+
+        for member in self.groups[group_id]:
+            address = member.split("@")
+            username_to = address[0].strip()
+            server_to = address[1].strip()
+            s_to = self.get_user(username_to)
+            if s_to is False or server_to not in self.servers or s_to is s_fr:
+                continue
+            if server_to == server_from:
+                self.send_message(sessionid, username_from, username_to, message)
+                sent_id.append(username_to)
+            else:
+                self.send_to_other_server(sessionid, server_to, username_to, message)
+                sent_id.append(s_to['nama'])
+
+        return {'status': 'OK', 'message': 'Message Sent to {} in {}'.format(', '.join(sent_id), group_id)}
+
+    def get_inbox(self, username):
+        s_fr = self.get_user(username)
+        incoming = s_fr['incoming']
+        msgs = {}
+        for users in incoming:
+            msgs[users] = []
+            while not incoming[users].empty():
+                msgs[users].append(s_fr['incoming'][users].get_nowait())
+        return {'status': 'OK', 'messages': msgs}
+
+    def server_inbox(self, username_from, username_to, message):
+        s_fr = self.get_user(username_from)
+        s_to = self.get_user(username_to)
+        if s_fr == False or s_to == False:
+            return {'status': 'ERROR', 'message': 'User Tidak Ditemukan'}
+        message = {'msg_from': s_fr['nama'], 'msg_to': s_to['nama'], 'msg': message}
+        outqueue_sender = s_fr['outgoing']
+        inqueue_receiver = s_to['incoming']
+        try:
+            outqueue_sender[username_from].put(message)
+        except KeyError:
+            outqueue_sender[username_from] = Queue()
+            outqueue_sender[username_from].put(message)
+        try:
+            inqueue_receiver[username_from].put(message)
+        except KeyError:
+            inqueue_receiver[username_from] = Queue()
+            inqueue_receiver[username_from].put(message)
+        return {'status': 'OK', 'message': 'Message Sent'}
+
+    def send_to_other_server(self, sessionid, server_id, username_to, message):
+        if sessionid not in self.sessions:
+            return {'status': 'ERROR', 'message': 'Session Tidak Ditemukan'}
+        if server_id not in self.servers:
+            return {'status': 'ERROR', 'message': 'Server Tidak Ada'}
+        username_from = self.sessions[sessionid]['username']
+        self.servers[server_id].put('server_inbox {} {} {}'.format(username_from, username_to, message))
+        return {'status': 'OK', 'message': 'Message Sent to Server'}
+
+    def send_file(self, sessionid, username_to, server_id, filename):
+        if sessionid not in self.sessions:
+            return {'status': 'ERROR', 'message': 'Session Tidak Ditemukan'}
+        if username_to not in self.users:
+            return {'status': 'ERROR', 'message': 'User Tujuan Tidak Ditemukan'}
+        if server_id not in self.servers:
+            return {'status': 'ERROR', 'message': 'Server Tidak Ada'}
+        username_from = self.sessions[sessionid]['username']
+        path_from = 'files/' + username_from + '/'
+        path_to = '../' + username_to + '/'
+        print(os.path.abspath(path_from))
+        os.chdir(path_from)
+
+        if os.path.exists(filename):
+            fp = open(f"{filename}", 'rb')
+            isi_file = fp.read()
+            fp.close()
+            os.chdir(path_to)
+            if os.path.exists(f"{filename}"):
+                os.chdir('../../')
+                return {'status': 'ERROR', 'message': 'File sudah ada di user {}'.format(username_to)}
+            fp_new = open(filename, 'wb+')
+            fp_new.write(isi_file)
+            fp_new.close()
+            os.chdir('../../')
+            return {'status': 'OK', 'message': 'File terkirim'}
+        else:
+            os.chdir('../../')
+            return {'status': 'ERROR', 'message': 'File tidak ada'}
+
+    def send_file_group(self, sessionid, groupid, filename):
+        if sessionid not in self.sessions:
+            return {'status': 'ERROR', 'message': 'Session Tidak Ditemukan'}
+        if groupid not in self.groups:
+            return {'status': 'ERROR', 'message': 'Group Tidak Ditemukan'}
+
+        username_from = self.sessions[sessionid]['username']
+        path_from = 'files/' + username_from + '/'
+
+        for member in self.groups[groupid]:
+            address = member.split("@")
+            username_to = address[0].strip()
+            if username_from == username_to:
+                continue
+            server_to = address[1].strip()
+            path_to = '../' + username_to + '/'
+            print(os.path.abspath(path_from))
+            os.chdir(path_from)
+
+            if os.path.exists(filename):
+                fp = open(f"{filename}", 'rb')
+                isi_file = fp.read()
+                fp.close()
+                os.chdir(path_to)
+                if os.path.exists(f"{filename}"):
+                    os.chdir('../../')
+                    return {'status': 'ERROR', 'message': 'File sudah ada di user {}'.format(username_to)}
+                fp_new = open(filename, 'wb+')
+                fp_new.write(isi_file)
+                fp_new.close()
+                os.chdir('../../')
+            else:
+                os.chdir('../../')
+                return {'status': 'ERROR', 'message': 'File tidak ada'}
+        return {'status': 'OK', 'message': 'File terkirim ke group {}'.format(groupid)}
+
+    def create_group(self, group_id, members):
+        if group_id in self.groups:
+            return {'status': 'ERROR', 'message': 'Group {} sudah ada'.format(group_id)}
+        self.groups[group_id] = []
+        user_member = members.split(',')
+        for user in user_member:
+            if user not in self.groups[group_id]:
+                self.groups[group_id].append(user)
+
+        return {'status': 'OK', 'message': 'Group {} dibuat'.format(group_id)}
+
+
+if __name__ == "__main__":
+    j = Chat()
---
+++ chatcli.py
@@ -0,0 +1,238 @@
+import socket
+import os
+import sys
+import json
+
+TARGET_IP = "127.0.0.1"
+
+class ChatClient:
+    def __init__(self, server):
+        self.server = server
+        self.portnumber = 8889
+        if server == 'A':
+            self.portnumber = 8889
+        elif server == 'B':
+            self.portnumber = 9000
+        elif server == 'C':
+            self.portnumber = 9001
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_address = (TARGET_IP, self.portnumber)
+        self.sock.connect(self.server_address)
+        self.tokenid = ""
+
+    def proses(self, cmdline):
+        j = cmdline.split(" ")
+        try:
+            command = j[0].strip()
+            if command == 'auth':
+                username = j[1].strip()
+                password = j[2].strip()
+                return self.login(username, password)
+            elif command == 'connect':
+                server_id = j[1].strip()
+                return self.connect(server_id)
+            elif command == 'logout':
+                return self.logout()
+            elif command == 'addserver':
+                server_id = j[1].strip()
+                server_ip = j[2].strip()
+                server_port = int(j[3].strip())
+                return self.add_server(server_id, server_ip, server_port)
+            elif command == 'send':
+                address = j[1].strip().split('@')
+                usernameto = address[0].strip()
+                serverto = address[1].strip()
+                message = ""
+                for w in j[2:]:
+                    message = "{} {}".format(message, w)
+                if serverto == self.server:
+                    return self.send_message(usernameto, message)
+                else:
+                    return self.send_message_to_server(serverto, usernameto, message)
+            elif command == 'sendgroup':
+                group_id = j[1].strip()
+                message = ""
+                for w in j[2:]:
+                    message = "{} {}" . format(message, w)
+                return self.sendgroupmessage(group_id, message)
+            elif command == 'sendfile':
+                address = j[1].strip()
+                filename = j[2].strip()
+                return self.send_file(address, filename)
+            elif command == 'sendfilegroup':
+                groupid = j[1].strip()
+                filename = j[2].strip()
+                return self.send_file_group(groupid, filename)
+            elif command == 'inbox':
+                return self.inbox()
+            elif command == 'register':
+                username = j[1].strip()
+                password = j[2].strip()
+                return self.register(username, password)
+            elif command == 'creategroup':
+                group_id = j[1].strip()
+                group_members = j[2].strip()
+                return self.create_group(group_id, group_members)
+            else:
+                return "*Maaf, command tidak benar"
+        except IndexError:
+            return "-Maaf, command tidak benar"
+
+    def sendstring(self, string):
+        try:
+            self.sock.sendall(string.encode())
+            receivemsg = ""
+            while True:
+                data = self.sock.recv(64)
+                if data:
+                    receivemsg = "{}{}" . format(receivemsg, data.decode())
+                    if receivemsg[-4:] == '\r\n\r\n':
+                        return json.loads(receivemsg)
+        except:
+            self.sock.close()
+            return {'status': 'ERROR', 'message': 'Gagal'}
+
+    def register(self, username, password):
+        message = "register {} {}\r\n".format(username, password)
+        result = self.sendstring(message)
+        if result['status'] == 'OK':
+            return 'User {} succesfully registered'.format(username)
+        else:
+            return 'Error: {}'.format(result['message'])
+
+    def login(self, username, password):
+        if self.tokenid != '':
+            return "Error: another user already logged in"
+        string = "auth {} {} \r\n" . format(username, password)
+        result = self.sendstring(string)
+        if result['status'] == 'OK':
+            self.tokenid = result['tokenid']
+            return "username {} logged in, token {} " .format(username, self.tokenid)
+        else:
+            return "Error: {}" . format(result['message'])
+
+    def logout(self):
+        string = "logout {}\r\n".format(self.tokenid)
+        result = self.sendstring(string)
+        if result['status'] == 'OK':
+            self.tokenid = ''
+            return "Logged out"
+        else:
+            return "Error: {}".format(result['message'])
+
+    def connect(self, server_id):
+        if self.tokenid == "":
+            return "Error, not authorized"
+        message = "connect {} \r\n".format(server_id)
+        result = self.sendstring(message)
+        if result['status'] == 'OK':
+            return 'Server {} succesfully connected'.format(server_id)
+        else:
+            return "Error: {}".format(result['message'])
+
+    def add_server(self, server_id, server_ip, server_port):
+        if self.tokenid == "":
+            return "Error: not authorized"
+        string = "addserver {} {} {}\r\n".format(server_id, server_ip, server_port)
+        result = self.sendstring(string)
+        if result['status'] == 'OK':
+            return "Server {}:{} added with id: {}".format(server_ip, server_port, server_id)
+        else:
+            return "Error: {}".format(result['message'])
+
+    def send_message(self, usernameto="xxx", message="xxx"):
+        if self.tokenid == "":
+            return "Error: not authorized"
+        string = "send {} {} {}\r\n" . format(
+            self.tokenid, usernameto, message)
+        result = self.sendstring(string)
+        if result['status'] == 'OK':
+            return "message sent to {}" . format(usernameto)
+        else:
+            return "Error: {}" . format(result['message'])
+
+    def send_message_to_server(self, serverid, usernameto, message):
+        if self.tokenid == "":
+            return "Error, not authorized"
+        string = "sendserver {} {} {} {}\r\n".format(self.tokenid, serverid, usernameto, message)
+        result = self.sendstring(string)
+        if result['status'] == 'OK':
+            return "Message sent to server {}".format(serverid)
+        else:
+            return "Error: {}".format(result['message'])
+
+    def inbox(self):
+        if self.tokenid == "":
+            return "Error, not authorized"
+
+        string = "inbox {}\r\n".format(self.tokenid)
+        result = self.sendstring(string)
+
+        if result['status'] == 'OK':
+            formatted_string = ""
+
+            for sender, messages in result['messages'].items():
+                for message in messages:
+                    formatted_message = message["msg"].replace("\r\n", "").strip()
+                    formatted_string += f"{sender}: {formatted_message}\n"
+
+            return "{}".format(formatted_string)
+        else:
+            return "Error: {}".format(result['message'])
+
+    def sendgroupmessage(self, group_id="xxx", message="xxx"):
+        if self.tokenid == "":
+            return "Error: not authorized"
+        string = "sendgroup {} {} {} {}\r\n" . format(self.tokenid, group_id, self.server, message)
+        result = self.sendstring(string)
+        if result['status'] == 'OK':
+            return "Message sent to {}" . format(group_id)
+        else:
+            return "Error: {}" . format(result['message'])
+
+    def send_file(self, address, filename):
+        if self.tokenid == "":
+            return "Error: not authorized"
+        string = "sendfile {} {} {}\r\n" . format(self.tokenid, address, filename)
+        result = self.sendstring(string)
+        if result['status'] == 'OK':
+            return 'File {} sent to {}'.format(filename, address)
+        else:
+            return 'Error: {}'.format(result['message'])
+
+    def send_file_group(self, group_id, filename):
+        if self.tokenid == "":
+            return "Error: not authorized"
+        string = "sendfilegroup {} {} {}\r\n".format(self.tokenid, group_id, filename)
+        result = self.sendstring(string)
+        if result['status'] == 'OK':
+            return 'File {} sent to group {}'.format(filename, group_id)
+        else:
+            return 'Error: {}'.format(result['message'])
+
+    def create_group(self, group_id, members):
+        if self.tokenid == "":
+            return "Error: not authorized"
+        string = "creategroup {} {}\r\n".format(group_id, members)
+        result = self.sendstring(string)
+        if result['status'] == 'OK':
+            return 'Group {} created with member: {}'.format(group_id, members)
+        else:
+            return 'Error: {}'.format(result['message'])
+
+
+if __name__ == "__main__":
+    server = 'A'
+    try:
+        server = sys.argv[1]
+    except:
+        pass
+
+    if server != 'A' and server != 'B' and server != 'C':
+        print("Server tidak ada!")
+        sys.exit()
+
+    cc = ChatClient(server)
+    while True:
+        cmdline = input("\nCommand: ")
+        print(cc.proses(cmdline))
